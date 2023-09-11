e2c61fa890b5bee459a4acf8fdc99de7b934411b
viniciusbls9 <beneditoloura@gmail.com>
2023-09-11 08:24:39 -0300

    feat: create GetEnv func util

---
+++ pkg/utils/utils.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func GetEnv() string {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the env")
+		return ""
+	}
+
+	return dbURL
+}
